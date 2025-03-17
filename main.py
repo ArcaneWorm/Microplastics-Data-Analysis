9819dbc0735be02c5f16531d180700a96b591f9c
BroncoJimmy <landonyaya@gmail.com>
2025-03-16 21:18:31 -0700

    Fix units. Create 2 bar charts. Need verification.

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import pandas as pd
+import time
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
 
+ms = time.time()
 df = pd.read_csv("samples_geocoded.csv")
 # list of unneeded columns to remove
 cols_to_remove = [
@@ -27,6 +32,7 @@ cols_to_remove = [
     "Size_1_50um", "Size_1_10um", "Size_1_5um", "Size_110_124nm", "Size_0_20um",
 
     "Approximate_Latitude", "Approximate_Longitude" ]
+
 df.drop(columns=cols_to_remove, axis=1, inplace=True)
 
 # remove NA values
@@ -34,3 +40,71 @@ df.dropna(axis=0, subset=["Countries", "Concentration", "Source"], how="any", in
 
 df.info() # info about dataframe
 
+# Convert 'Concentration' column to numeric (in order to remove those that are not a number)
+df["Concentration"] = pd.to_numeric(df["Concentration"], errors="coerce")
+
+# Drop rows where 'Concentration' is NaN
+df = df.dropna(subset=["Concentration"])
+
+# Drop rows where Concentration_Units is not in particles/volume
+# TODO Discuss whether this is the right option.
+df = df[~df["Concentration_Units"].isin(["ug/m3", "ug/g"])]
+
+i = 0
+concentrations = df["Concentration"].to_list()
+sources=df["Source"].to_list()
+bottled_avg=0
+tap_avg=0
+# Unit Conversions TODO Verify
+for x in df["Concentration_Units"].to_list():
+    if x == "particles/0.33L":
+        concentrations[i] = concentrations[i]*3
+    elif x == "particles/m3":
+        concentrations[i] = concentrations[i]/1000
+    elif x == "particles/50 L":
+        concentrations[i] = concentrations[i]/50
+    elif x == "particles/50 L":
+        concentrations[i] = concentrations[i]/50
+    elif x == "particles/bottle":
+        # Assuming an average bottle holds around .5 liter
+        concentrations[i] = concentrations[i]*2
+    
+    # Add to averages for bar charts
+    if sources[i] == "bottled water":
+        bottled_avg+=concentrations[i]
+    else:
+        tap_avg+=concentrations[i]
+    i+=1
+    
+bottled_avg = bottled_avg/df["Source"].size
+tap_avg = tap_avg/df["Source"].size
+print("bottled avg",bottled_avg)
+print("tap avg",tap_avg)
+print()
+
+# Replace with new converted units, all now in particles/L
+df["Concentration"] = concentrations
+#df["Concentration_Units"] = "particles/L"
+
+n = (time.time() - ms)
+print("In seconds:",n)
+
+# Bar chart comparing bottled vs tap water
+plt.figure(figsize=(6, 6))
+#plt.bar(df["Source"], df["Concentration"])
+plt.bar(df["Countries"], df["Concentration"])
+plt.title('Countries and Concentration')
+plt.xlabel('Country')
+plt.ylabel('Concentration (particles/Liter)')
+plt.show()
+
+df.to_csv("output.csv",index=False)
+
+x = ['Tap', 'Bottled']
+y = [tap_avg, bottled_avg]
+
+plt.bar(x, y)
+plt.title('Tap vs Bottled water contaminant amount')
+plt.xlabel('Water Source')
+plt.ylabel('Particles/Liter')
+plt.show()
